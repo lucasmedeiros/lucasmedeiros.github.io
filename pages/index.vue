49695ee07e5737f0ed05e0955f0e8f026fcc539c
LukeHxH <lucasmed812@gmail.com>
2019-03-20 00:26:03 -0300

    Finaliza introdução

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,15 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">
-        Portfolio
-      </h1>
-      <h2 class="subtitle">
-        <a>lukehxh.github.io/portfolio</a>
-      </h2>
-    </div>
-  </section>
+  <div id="app">
+    <Intro />
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Intro from '~/components/Intro.vue'
 
 export default {
   components: {
-    Logo
+    Intro
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-</style>
