f10fbf0eb3aaca6e5559b7bfef902e2b74aa071b
Lucas Medeiros <lucasmed812@gmail.com>
2020-04-23 06:47:01 -0300

    Add Twitter social media

--- config/metadata.js
+++ config/metadata.js
@@ -7,25 +7,30 @@ module.exports = {
     {
       name: "Github",
       url: "https://www.github.com/lucasmedeiros/",
-      icon: "github"
+      icon: "github",
     },
     {
       name: "Dev",
       url: "https://dev.to/lukehxh",
-      icon: "dev"
+      icon: "dev",
     },
     {
       name: "LinkedIn",
       url: "https://www.linkedin.com/in/lucasmnf/",
-      icon: "linkedin"
+      icon: "linkedin",
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com/_lucasmnf",
+      icon: "twitter",
     },
     {
       name: "YouTube",
       url: "https://www.youtube.com/channel/UCLlxw0ys56q3t0JJhPOw4-w/videos",
-      icon: "youtube"
-    }
+      icon: "youtube",
+    },
   ],
   email: ["lucasmed812@gmail.com", "lucas.medeiros.fernandes@ccc.ufcg.edu.br"],
   whatsapp: "+55 (84) 99921-3854",
-  location: "Campina Grande, Brazil"
+  location: "Campina Grande, Brazil",
 };
--- src/components/custom/social.js
+++ src/components/custom/social.js
@@ -3,7 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default ({ data, size = "2x", color = "white" }) =>
   data.map((social, index) => (
-    <a key={index} href={social.url} style={{ margin: "0 10px 0 10px" }}>
+    <a
+      key={index}
+      href={social.url}
+      style={{ margin: "0 10px 0 10px" }}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <FontAwesomeIcon icon={["fab", social.icon]} color={color} size={size} />
     </a>
   ));
